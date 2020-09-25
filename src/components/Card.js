9c4af345d3caab6f9063e9b4a0e722d643ea7ddc
Roman Franklin <romankfranklin213@icloud.com>
2020-09-25 11:17:32 -0400

    added Card.js to wireframes

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React from `react`;
+import {View, Text, Image} from "react-native";
+
+export default () => {
+    return (
+        <View>
+            <Image
+                source ={require("../assests/icon.png")}
+                />
+                <View>
+                    <Texts>[title]</Texts>
+                    <Text>
+                        
+                    
+                    </Text>
+                </View>
+
+
+
+
+        </View>
+
+
+    )
+
+
+}
