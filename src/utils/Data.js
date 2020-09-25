99ffd417c112fc9797e4d22787f365ec9417cc54
Roman Franklin <romankfranklin213@icloud.com>
2020-09-25 11:16:12 -0400

    added the data to wireframes

---
+++ src/utils/Data.js
@@ -0,0 +1,27 @@
+export default Data = [
+  {
+    id: "1",
+    title: "The Red Ball",
+    body:
+      "The red ball sat proudly at the top of the toybox. It had been the last to be played with and anticipated it would be the next as well. The other toys grumbled beneath. At one time each had held the spot of the red ball, but over time they had sunk deeper and deeper into the toy box.",
+  },
+
+  {
+    id: "2",
+    title: "She sat",
+    body:
+      "She sat in the darkened room waiting. It was now a standoff. He had the power to put her in the room, but not the power to make her repent. It wasn't fair and no matter how long she had to endure the darkness, she wouldn't change her attitude. At three years old, Sandy's stubborn personality had already bloomed into full view.",
+  },
+  {
+    id: "3",
+    title: "He Looked",
+    body:
+      "He looked at the sand. Picking up a handful, he wondered how many grains were in his hand. Hundreds of thousands? Not enough,the said under his breath. I need more.",
+  },
+  {
+    id: "4",
+    title: "Weird concepts",
+    body:
+      "It was a weird concept. Why would I really need to generate a random paragraph? Could I actually learn something from doing so? All these questions were running through her head as she pressed the generate button. To her surprise, she found what she least expected to see.",
+  },
+];
